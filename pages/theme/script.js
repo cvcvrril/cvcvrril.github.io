edf031b63d724f6cc9f3919c80ada001c56380fc
ines (casa) <imartinezrodriguez@educa.madrid.org>
2025-01-21 22:51:43 +0100

    created about.html

--- pages/theme/script.js
+++ pages/theme/script.js
@@ -24,16 +24,3 @@ fetch("pages/footer.html")
     .then(data => {
         document.getElementById("footer").innerHTML = data;
     });
-
-/*
-document.getElementById("color-mode").addEventListener("click", function() {
-    document.body.classList.toggle("dark-mode");
-    const icon = document.querySelector(".material-icons");
-
-    if (document.body.classList.contains("dark-mode")) {
-        icon.textContent = "light_mode";
-    } else {
-        icon.textContent = "dark_mode";
-    }
-});
-*/
