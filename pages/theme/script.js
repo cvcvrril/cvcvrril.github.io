b9f69ff2ce71d32a47fac80347b2e7dde212df56
ines (casa) <imartinezrodriguez@educa.madrid.org>
2025-01-25 00:14:18 +0100

    update about.html + sidebar.html + fixed script.js

--- pages/theme/script.js
+++ pages/theme/script.js
@@ -4,12 +4,23 @@ fetch("pages/sidebar.html")
         document.getElementById("sidebar").innerHTML = data;
 
         const colorModeButton = document.getElementById("color-mode");
+
+        const isDarkMode = localStorage.getItem("darkMode") === "true";
+        if (isDarkMode) {
+            document.body.classList.add("dark-mode");
+            const icon = document.querySelector(".material-icons");
+            if (icon) icon.textContent = "light_mode";
+        }
+
         if (colorModeButton) {
             colorModeButton.addEventListener("click", function () {
                 document.body.classList.toggle("dark-mode");
                 const icon = document.querySelector(".material-icons");
 
-                if (document.body.classList.contains("dark-mode")) {
+                const isDarkMode = document.body.classList.contains("dark-mode");
+                localStorage.setItem("darkMode", isDarkMode);
+
+                if (isDarkMode) {
                     icon.textContent = "light_mode";
                 } else {
                     icon.textContent = "dark_mode";
